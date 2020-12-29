c16544494203b2d8b9c9154533a6cfdd7c61fc3c
sezg <sezgandhi@gmail.com>
2020-12-29 12:44:46 -0500

    Initial Commit

---
+++ SaveMe/SaveMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveMeApp.swift
+//  SaveMe
+//
+//  Created by Sagar Gandhi on 2020-12-29.
+//
+
+import SwiftUI
+
+@main
+struct SaveMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
